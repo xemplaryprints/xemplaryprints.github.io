f33a7be6a067812989dae9c592c9f293018ac332
Samuel Marin <120614368+xemplaryprints@users.noreply.github.com>
2023-03-05 19:23:44 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-let popup = document.getElementById("popup");
-
-function openPopup(){
-    popup.classList.add("open-popup");
-}
-
-function mainPage(){
-    popup.classList.remove("open-popup");
-}
