3c7f6b0036933726f87c81d5ad634202838279af
Samuel Marin <120614368+xemplaryprints@users.noreply.github.com>
2023-03-14 21:59:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let menuList = document.getElementById("menuList");
-
 function toggleMenu(){
     menuList.classList.add("menu-container");
+    showMenu.classList.remove("showMenu");
+    hideMenu.classList.remove("close-menu");
 }
+
+
+function closeMenu(){
+    hideMenu.classList.add("close-menu");
+}
+
+
+
+//const hide = document.querySelector(".menu-container")
+
+//document.addEventListener("click", function(event){
+//    if (event.target.closest(".menu-container")) return
+//    menuList.classList.add("hide-menu");
+//})
